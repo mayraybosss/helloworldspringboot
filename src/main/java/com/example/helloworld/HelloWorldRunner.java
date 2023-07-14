6af58afe7cd0d159f97ba28ab1e732f9bd473a53
Yan_Nizhnik <mayraybosss@gmail.com>
2023-07-14 10:09:08 +0200

    helloworld spring

---
+++ src/main/java/com/example/helloworld/HelloWorldRunner.java
@@ -0,0 +1,13 @@
+package com.example.helloworld;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloWorldRunner implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("hello world");
+    }
+}
